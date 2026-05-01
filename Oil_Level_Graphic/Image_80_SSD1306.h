0578e430c98773e789d86b17bb55c489d8aedfc9
tobiaslenz1988 <tobias.lenz88@live.de>
2026-05-01 18:08:41 +0200

    Update and rename Image_80.h to Image_80_SSD1306.h
    m

--- Oil_Level_Graphic/Image_80_SSD1306.h
+++ Oil_Level_Graphic/Image_80_SSD1306.h
@@ -17,7 +17,9 @@
 *  line scan direction: forward
 *  inverse: no
 *******************************************************************************/
-
+/*
+This File is for a 128*64 Bit SSD1306 Display
+*/
 /*
  typedef struct {
      const uint8_t *data;
