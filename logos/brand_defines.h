377bdbf11ac63c6d4eddcae9c9aa8fb8926c7fdc
tobiaslenz1988 <tobias.lenz88@live.de>
2026-05-14 00:20:46 +0200

    Add files via upload

---
+++ logos/brand_defines.h
@@ -0,0 +1,14 @@
+#ifndef BRAND_DEFINES_H
+#define BRAND_DEFINES_H
+
+#define BRAND_Init					  0x00
+#define BRAND_AUDI					  0x01
+#define BRAND_AUDI_QUATTRO			  0x02
+#define BRAND_AUDI_1_8T				  0x03
+#define BRAND_VW				      0x04
+#define BRAND_CHEVY					  0x05
+#define	BRAND_NISSAN_GTT			  0x06
+#define BRAND_NISSAN_Skyline	  	  0x07
+
+
+#endif /*  BRAND_DEFINES_H */
